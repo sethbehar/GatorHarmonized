698ad8a20b68d6baaeb17d965d79203ef62ee400
Seth Behar <61632271+sethbehar@users.noreply.github.com>
2023-04-20 14:24:20 -0400

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,35 +1,94 @@
-//
-// Created by jakem on 4/19/2023.
-//
-
-#ifndef GATORHARMONIZED_GRAPH_H
-#define GATORHARMONIZED_GRAPH_H
-
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
 #include <vector>
+#include <queue>
+#include <map>
+#include <set>
 #include "Artist.h"
+
 using namespace std;
 
-//We will be implementing the graph using an adjacency matrix, since every node is connected.
-struct Graph{
-    //Artists and Genre vectors.
-    vector<Artist> artists;
-    vector<string> genres;
-    //Adjacency matrix to hold the edge weights / similarity scores.
-    vector<vector<double>> scores;
+struct Edge {
+    Artist* a;
+    Artist* b;
+    float weight;
+};
 
-    Graph(){
+class EdgeComparator {
+public:
+    bool operator()(const Edge& a, const Edge& b) {
+        return a.weight > b.weight;
+    }
+};
 
+class MinimumSpanningTree {
+public:
+    MinimumSpanningTree(vector<Artist*>& artists) {
+        int n = artists.size();
+        vector<vector<float>> similarityMatrix(n, vector<float>(n, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    //float similarityScore = CalculateSimilarity(artists[i], artists[j]);
+                    //similarityMatrix[i][j] = similarityScore;
+                    cout << artists[i]->familiarity << endl;
+                }
+            }
+        }
+
+        set<int> visitedNodes;
+        priority_queue<Edge, vector<Edge>, EdgeComparator> pq;
+
+        visitedNodes.insert(0);
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                Edge e;
+                e.a = artists[0];
+                e.b = artists[i];
+                e.weight = similarityMatrix[0][i];
+                pq.push(e);
+            }
+        }
+
+        while (visitedNodes.size() < n) {
+            Edge e = pq.top();
+            pq.pop();
+
+            if (visitedNodes.find(getIndex(artists, e.a)) == visitedNodes.end() || visitedNodes.find(getIndex(artists, e.b)) == visitedNodes.end()) {
+                visitedNodes.insert(getIndex(artists, e.a));
+                visitedNodes.insert(getIndex(artists, e.b));
+                minimumSpanningTree.push_back(e);
+                int nodeToCheck;
+
+                if (visitedNodes.find(getIndex(artists, e.a)) == visitedNodes.end()) {
+                    nodeToCheck = getIndex(artists, e.a);
+                }
+                else {
+                    nodeToCheck = getIndex(artists, e.b);
+                }
+
+                for (int i = 0; i < n; i++) {
+                    if (visitedNodes.find(i) == visitedNodes.end()) {
+                        Edge newEdge;
+                        newEdge.a = artists[nodeToCheck];
+                        newEdge.b = artists[i];
+                        newEdge.weight = similarityMatrix[nodeToCheck][i];
+                        pq.push(newEdge);
+                    }
+                }
+            }
+        }
     }
 
-    void add(Artist artist){
-
+    vector<Edge> getMinimumSpanningTree() {
+        return minimumSpanningTree;
     }
 
-    double CalculateSimilarity(Artist A1, Artist A2){
+private:
+    vector<Edge> minimumSpanningTree;
+
+    double CalculateSimilarity(Artist* A1, Artist* A2) {
 
         /*Our algorithm for calculating similaty scores is simple:
          * There are three factors that contribute, familiarity score, hotness score, and genre.
@@ -40,11 +99,11 @@ struct Graph{
          * Lastly we can compare artist hotness which will add a maximum of 0.25 points to the score.
          * */
 
-        //First, we need to convert familiarity and hotness to double values.
-        double famA1 = stod(A1.familiarity);
-        double famA2 = stod(A2.familiarity);
-        double hotA1 = stod(A1.hottness);
-        double hotA2 = stod(A2.hottness);
+         //First, we need to convert familiarity and hotness to double values.
+        double famA1 = stod(A1->familiarity);
+        double famA2 = stod(A2->familiarity);
+        double hotA1 = stod(A1->hottness);
+        double hotA2 = stod(A2->hottness);
 
         //Next we will calculate the genre score.
         double genreScore = 0;
@@ -53,7 +112,7 @@ struct Graph{
         vector<string> words;
 
         //Parse the gnere by spaces.
-        string s = A1.genre;
+        string s = A1->genre;
         stringstream ss(s);
         string word;
         while (ss >> word) {
@@ -62,24 +121,24 @@ struct Graph{
 
         //Now see if A2 has the genre of A1 within it.
         bool contains = false;
-        string temp = A2.genre;
+        string temp = A2->genre;
         for (int i = 0; i < words.size(); i++) {
-            if(temp.find(words[i]) != 0){
+            if (temp.find(words[i]) != 0) {
                 contains = true;
             }
         }
         //If the genre is contained in A2
-        if(contains){
+        if (contains) {
             genreScore += 0.4;
         }
 
         //Next we need to calculate the familiarity scores using our algorithm.
         //Our method will prevent famScore from rising over the 0.35 threshold.
         double famScore = 0;
-        if (famA1 > famA2){
+        if (famA1 > famA2) {
             famScore = 0.35 * (1 + ((famA2 / famA1) * 0.5));
         }
-        else{
+        else {
             famScore = 0.35 * (1 + ((famA1 / famA2) * 0.5));
         }
 
@@ -90,7 +149,7 @@ struct Graph{
         if (hotA1 > hotA2) {
             hotScore = 0.25 * (1 + ((hotA2 / hotA1) * 0.3));
         }
-        else{
+        else {
             hotScore = 0.25 * (1 + ((hotA1 / hotA2) * 0.3));
         }
 
@@ -100,6 +159,14 @@ struct Graph{
         //Finally return the score to be used when crafting the adjacency matrix.
         return score;
     }
-};
 
-#endif //GATORHARMONIZED_GRAPH_H
+    int getIndex(vector<Artist*>& artists, Artist* a) {
+        for (int i = 0; i < artists.size(); i++) {
+            if (artists[i] == a) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
