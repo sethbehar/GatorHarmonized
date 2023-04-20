d25c67fb7611664d37598e19d4daafb9d11e5951
Seth Behar <61632271+sethbehar@users.noreply.github.com>
2023-04-20 14:24:36 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include "Artist.h"
+#include "Graph.h"
 #include "CSV.h"
 
 using namespace std;
@@ -11,19 +12,33 @@ using namespace std;
 int main() {
     CsvParser parser("music.csv");
     vector<vector<string>> csv = parser.getData();
-    vector<Artist> artists;
+    vector<Artist*> artists;
 
     for (int i = 0; i < csv.size(); i++) {
         // Create new artist object, only passing in the values -> id, name, familiarity, hottness, longitude
-        Artist artist(csv[i][2], csv[i][6], csv[i][0], csv[i][1], csv[i][5], csv[i][8]);
-        artists.push_back(artist);
-    }
+        Artist* artist = new Artist(csv[i][2], csv[i][6], csv[i][0], csv[i][1], csv[i][5], csv[i][8]);
+        artist->id = csv[i][2];
+        artist->name = csv[i][6];
+        artist->familiarity = csv[i][0];
+        artist->hottness = csv[i][1];
+        artist->longitude = csv[i][5];
+        artist->genre = csv[i][8];
 
-    for (int i = 0; i < artists.size(); i++) {
-        artists[i].printArtist();
-        cout << endl;
+
+        artists.push_back(artist);
+        //artist.printArtist();
     }
+    MinimumSpanningTree mst(artists);
+    //vector<Edge> minimumSpanningTree = mst.getMinimumSpanningTree();
+    float totalWeight = 0;
+
+    /*for (int i = 0; i < minimumSpanningTree.size(); i++) {
+        Edge e = minimumSpanningTree[i];
+        cout << e.a->getName() << " -- " << e.b->getName() << " : " << e.weight << endl;
+        totalWeight += e.weight;
+    }
+    cout << "Total weight: " << totalWeight << endl;*/
+    
 
     return 0;
 }
-
